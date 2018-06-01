ec0e34b8a1c2acd03f69b0ec98b00f573e70d0d9
Sam_Grinis <sam.grinis@gmail.com>
2018-06-01 16:29:10 -0700

    final

--- 08-routing/src/js/components/app.js
+++ 08-routing/src/js/components/app.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Catalog from './catalog/app-catalog'
-import Cart from './cart/app-cart'
-import Template from './app-template';
-import { Router, Route, IndexRoute } from 'react-router';
+import React from "react";
+import Catalog from "./catalog/app-catalog";
+import Cart from "./cart/app-cart";
+import Template from "./app-template";
+import { Router, Route, browserHistory, IndexRoute } from "react-router";
 
 export default () => {
   return (
-    <Router>
-      <Route path="/" component={ Template }>
-        <IndexRoute component={ Catalog } />
-        <Route path="cart" component={ Cart } />
+    <Router history={browserHistory}>
+      <Route path="/" component={Template}>
+        <IndexRoute component={Catalog} />
+        <Route path="cart" component={Cart} />
       </Route>
     </Router>
   );
-}
+};
--- src/js/main.js
+++
@@ -1,4 +0,0 @@
-import App from './components/app';
-import React from 'react';
-import ReactDOM from 'react-dom';
-ReactDOM.render(<App />, document.getElementById('app'));
